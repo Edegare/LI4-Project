21fc6c4ad35a02e7c6ace9888c69667315ab1b8e
Edgar Ferreira <edgarcferreira03@gmail.com>
2025-01-21 23:51:56 +0000

    Inicio de sessão com verificação de estado da conta + Mudança imagem de registo dos móveis

--- BMManager/FuncionarioDTO.cs
+++ BMManager/FuncionarioDTO.cs
@@ -7,12 +7,14 @@ public class FuncionarioDTO
     public int Id { get; set; }
     public string Name { get; set; }
     public string Equipa { get; set; }
+    public bool Conta_Ativa {  get; set; }
 
-    public FuncionarioDTO(int id, string name, string equipa)
+    public FuncionarioDTO(int id, string name, string equipa, bool conta_ativa)
     {
         Id = id;
         Name = name;
         Equipa = equipa;
+        Conta_Ativa = conta_ativa;
     }
 
     // Método para converter o DTO em uma lista de Claims
