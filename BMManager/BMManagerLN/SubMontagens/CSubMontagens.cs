fd124eb4981606baa70731cc53d4af5c7f1be09b
rafael-vilasboas <rafaelvilasboas0102@gmail.com>
2025-01-22 22:07:03 +0000

    Mensagens de erros de imagens no movel e etapas

--- BMManager/BMManagerLN/SubMontagens/CSubMontagens.cs
+++ BMManager/BMManagerLN/SubMontagens/CSubMontagens.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using BMManager.BMManagerCD;
+using BMManagerLN.SubMateriais;
+using BMManagerLN.SubMoveis;
 
 namespace BMManagerLN.SubMontagens
 {
--- BMManager/BMManagerUI/MovelTemp.cs
+++ BMManager/BMManagerUI/MovelTemp.cs
@@ -6,11 +6,8 @@ namespace BMManager.BMManagerUI
 {
     public class MovelTemp
     {
-        [Required(ErrorMessage = "O campo Nome é obrigatório!")]
-        [StringLength(75, ErrorMessage = "Nome não pode ter mais de 75 carateres!")]
         public string NovoMovelNome { get; set; }
 
-        [Required(ErrorMessage = "O campo Imagem é obrigatório!")]
         public byte[]? NovoMovelImagem { get; set; }
         public Dictionary<int, (byte[], int, List<(int,int)>)> NovoMovelEtapas { get; set; } = new Dictionary<int, (byte[], int, List<(int,int)>)>();
         public int EtapasRegistadas { get; set; } = 0;
